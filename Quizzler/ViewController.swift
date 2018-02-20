bc4d605aa23f2ba75a628d3c71b3b95cae6d4009
Max Shawndreck <me@shawndreck.com>
2018-02-20 13:16:18 +0800

    Implement displaying first question and printing answer

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer:Bool = false
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +22,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
   
+        if(sender.tag == 1){
+            pickedAnswer = true
+        }else if(sender.tag == 2){
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
     }
     
     
@@ -40,7 +51,13 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
+        let correctAnswer = allQuestions.list[0].answer
         
+        if(correctAnswer ==  pickedAnswer){
+            print("You got it!")
+        }else{
+            print("Wrong!")
+        }
     }
     
     
